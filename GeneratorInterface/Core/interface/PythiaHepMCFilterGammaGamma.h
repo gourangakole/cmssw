4bb1297e43f044d65c8930a7e02aebb9edb5fac4
Gouranga Kole <gouranga.tifr@gmail.com>
2018-04-06 17:11:41 +0200

    example em filter tested

---
+++ GeneratorInterface/Core/interface/PythiaHepMCFilterGammaGamma.h
@@ -0,0 +1,49 @@
+#ifndef PYTHIAHEPMCFILTERGAMMAGAMMA_h
+#define PYTHIAHEPMCFILTERGAMMAGAMMA_h
+
+//
+// Package:    GeneratorInterface/GenFilters
+// Class:      PythiaHepMCFilterGammaGamma
+// 
+// Original Author:  Matteo Sani
+//
+//
+
+#include "FWCore/Framework/interface/Frameworkfwd.h"
+#include "GeneratorInterface/Core/interface/BaseHepMCFilter.h"
+#include "FWCore/Framework/interface/EDFilter.h"
+
+#include "FWCore/Framework/interface/Event.h"
+#include "FWCore/Framework/interface/MakerMacros.h"
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
+
+namespace edm {
+	  class HepMCProduct;
+}
+
+#include "TH1D.h"
+#include "TH1I.h"
+
+class PythiaHepMCFilterGammaGamma : public BaseHepMCFilter {
+ public:
+  explicit PythiaHepMCFilterGammaGamma(const edm::ParameterSet&);
+  ~PythiaHepMCFilterGammaGamma();
+  
+  /** @return true if this GenEvent passes the double EM enrichment
+      criterion */
+  virtual bool filter(const HepMC::GenEvent* myGenEvent) override;
+ private:
+
+  const edm::EDGetTokenT<edm::HepMCProduct> token_;
+  const int maxEvents;
+
+  const double ptSeedThr, etaSeedThr, ptGammaThr, etaGammaThr, ptTkThr, etaTkThr;
+  const double ptElThr, etaElThr, dRTkMax, dRSeedMax, dPhiSeedMax, dEtaSeedMax, dRNarrowCone, pTMinCandidate1, pTMinCandidate2, etaMaxCandidate;
+  const double invMassMin, invMassMax;
+  const double energyCut;
+  const int nTkConeMax, nTkConeSum;
+  const bool acceptPrompts;
+  const double promptPtThreshold;
+
+};
+#endif
